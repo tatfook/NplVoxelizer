bc4366dacbc0cbe7304c8773e719902b242e167d
leio zhang <leio zhang>
2016-10-08 12:41:58 +0800

    changed namespace

--- InstallPackages.bat
+++ InstallPackages.bat
@@ -1,12 +1,24 @@
 @echo off 
-mkdir npl_packages
-cd npl_packages
- git clone https://github.com/NPLPackages/main
+if not exist "npl_packages" ( mkdir npl_packages )
 
-pushd main
-git pull 
+pushd "npl_packages"
+
+CALL :InstallPackage main
+CALL :InstallPackage paracraft
+CALL :InstallPackage NplCadLibrary
+CALL :InstallPackage ModelVoxelizer
 popd
 
-pushd paracraft
-git pull 
-popd
+EXIT /B %ERRORLEVEL%
+
+rem install function here
+:InstallPackage
+if exist "%1\README.md" (
+    pushd %1
+    git pull
+    popd
+) else (
+    rmdir /s /q "%CD%\%1"
+    git clone https://github.com/NPLPackages/%1
+)
+EXIT /B 0
